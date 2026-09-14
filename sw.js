9d77f26337a42bf6e4dc26fcbdc28e6c0fa662fd
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-14 22:19:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='alpine-team-supporter-v101';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./supporter-icon-192.png','./supporter-icon-512.png','./apple-touch-icon.png','./alpine-team-supporter-logo.png','./alpine-team-supporter-og.png'];
+const CACHE='alpine-team-supporter-v110';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./supporter-icon-192.png','./supporter-icon-512.png','./apple-touch-icon.png','./alpine-team-supporter-logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
